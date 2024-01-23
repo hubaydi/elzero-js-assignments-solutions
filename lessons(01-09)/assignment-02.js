eb64a76494cb651758c522ddd1d762205b3d2841
Cajaaib <138290252+cajaaib@users.noreply.github.com>
2024-01-23 16:20:37 +0300

    done

---
+++ lessons(01-09)/assignment-02.js
@@ -0,0 +1,9 @@
+// Method one
+document.write('<h1 style="color: blue; font-size: 80px; text-align: center; font-family: arial;">Elzero</h1>');
+
+// Method Two
+document.write('<h1>Elzero</h1>')
+document.querySelector('h1').style.color= 'blue';
+document.querySelector('h1').style.fontSize= '80px';
+document.querySelector('h1').style.textAlign= 'center';
+document.querySelector('h1').style.fontFamily= 'arial';
---
+++ lessons(01-09)/assignment-04.js
@@ -0,0 +1,15 @@
+console.group('Group One');
+console.log('Message one');
+console.log('Message two');
+console.group('Child group');
+console.log('Message one');
+console.log('Message two');
+console.group('Grand child group');
+console.log('Message one');
+console.log('Message two');
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group('Group two');
+console.log('Message one');
+console.log('Message two');
