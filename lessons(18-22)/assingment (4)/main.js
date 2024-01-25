2fa0773f86ec2ba084245d238935afedb9aba7fc
Cajaaib <138290252+cajaaib@users.noreply.github.com>
2024-01-25 17:11:57 +0300

    week three solutios with challenge

---
+++ lessons(18-22)/assingment (4)/main.js
@@ -0,0 +1,16 @@
+let points = 10;
+
+// i got no other solutions rather than incrementing and decrementing
+++points
+++points
+++points
+
+console.log(points); // 13
+
+--points
+--points
+--points
+--points
+--points
+
+console.log(points); // 8;
---
+++ lessons(18-22)/challenge/main.js
@@ -0,0 +1,30 @@
+/*
+  Challenge 1
+*/
+
+let a = 10;
+let b = "20";
+let c = 80;
+
+console.log(++a + +b++ + +c++ - +a++);
+console.log(++a + -b + +c++ - -a++ + +a);
+console.log(--c + +b + --a * +b++ - +b * a + --a - +true);
+
+/*
+  Hard
+*/
+
+/*
+  Challenge 2
+*/
+
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+
+// Only Use Variables Value
+// Do Not Use Variable Twice
+
+console.log(-d * +e); // 2000
+console.log(++e * ++g + -d + ++f); // 173
