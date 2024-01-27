2f947b7dbca8843633bd5c1babe97e7223b0d2cd
Cajaaib <138290252+cajaaib@users.noreply.github.com>
2024-01-27 09:30:36 +0300

    week four - part two

---
+++ lessons(26-30)/challenge/main.js
@@ -0,0 +1,22 @@
+/*
+  String Challenge
+  All Solutions Must Be In One Chain
+  You Can Use Concatenate
+*/
+
+let a = "Elzero Web School";
+
+// Include This Method In Your Solution [slice, charAt]
+console.log(a.charAt(2).toUpperCase() + a.slice(3, 6)); // Zero
+
+// 8 H
+console.log(a.substring(13, 14).toUpperCase().repeat(8)); // HHHHHHHH
+
+// Return Array
+console.log(a.split(" ", 1)); // ["Elzero"]
+
+// Use Only "substr" Method + Template Literals In Your Solution
+console.log(`${a.substr(0, 6)} ${a.substr(11)}`); // Elzero School
+
+// Solution Must Be Dynamic Because String May Change
+console.log(a.charAt(0).toLowerCase() + a.slice(1, -1).toUpperCase() + a.slice(-1)); // eLZERO WEB SCHOOl
